575a1cff1544c3cf06ad030af49da9e2ec914a93
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2023-12-25 10:18:09 -0800

    fix issues from comments of the team

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import {photoDescriptions} from "./data_constants.js"
-import {commentVariationsList} from "./data_constants.js"
-import {authorsNamesList} from "./data_constants.js"
+import {photoDescriptions, commentVariationsList, authorsNamesList} from "./data_constants.js"
+// import {commentVariationsList} from "./data_constants.js"
+// import {authorsNamesList} from "./data_constants.js"
 
 const randomInt = function (min, max) {
   min = Math.ceil(min)
@@ -23,33 +23,36 @@ const creatBulkOfComments = function (amountOfComments) {
       "name": "",
     }
     comment.id = ID()
-    comment.avatar = `../img/avatar-${counter}.svg`
-    comment.message = commentVariationsList[randomInt(0, commentVariationsList.length)]
+    comment.avatar = `../img/avatar-${counter}.svg` // change if we need more avatars
+    comment.message = commentVariationsList[randomInt(0, commentVariationsList.length)]  // check ends of array
     comment.name = authorsNamesList[randomInt(0, authorsNamesList.length)]
     comments.push(comment)
   }
   return comments
 }
 
-const createPhotoObjects = function (photoAmountMinusOne) {
+const createPhotoObjects = function (photoAmount) {
   const photoObjects = []
-  for (let counter = 0; counter <= photoAmountMinusOne; counter++) {
-    const photoObject = {
-      "id": "",
-      "url": "",
-      "description": "",
-      "likes": "",
-      "comments": "",
-    }
+  // for (let counter = 0; counter <= photoAmountMinusOne; counter++) {
+  for (let counter = 0; counter < photoAmount; counter++) {
+    const photoObject = {}  // consider to add into an object its values
+    // const photoObject = {
+    //   // "id": "",
+    //   "id": "",
+    //   "url": "",
+    //   "description": "",
+    //   "likes": "",
+    //   "comments": "",
+    // }
     photoObject.id = counter
     photoObject.url = `../photos/${counter + 1}.jpg`
     photoObject.description = photoDescriptions[counter]
-    photoObject.likes = randomInt(1, 33)
-    photoObject.comments = [...creatBulkOfComments(randomInt(1, 5))]
+    photoObject.likes = randomInt(1, 33)  // remove magic numbers
+    photoObject.comments = [...creatBulkOfComments(randomInt(1, 5))] // check end values
     photoObjects.push(photoObject)
   }
   return photoObjects
 }
 
-// const testObjects = createPhotoObjects(24)
-// console.log(testObjects)
+const testObjects = createPhotoObjects(24)
+console.log(testObjects)
