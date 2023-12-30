3aa4f2807d5a57d1a32afbee7da459d36cd25bd3
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2023-12-29 20:36:23 -0800

    optimized using dicument fragment

--- js/display_photos_thumbnails.js
+++ js/display_photos_thumbnails.js
@@ -1,26 +1,20 @@
-// fill one thumbnail with content from PhotoObject
-const displayThumbnail = function (photoObject) {
-  // finding template for our thumbnails
-  const thumbnailTemplate = document.querySelector("#picture")
-  // receiving content of the template
-  const pictureElement = thumbnailTemplate.cloneNode(true).content
-  // getting child elements
-  const pictureImg = pictureElement.querySelector(".picture__img")
-  const pictureComments = pictureElement.querySelector(".picture__comments")
-  const pictureLikes = pictureElement.querySelector(".picture__likes")
-  // mapping data from PhotoObject and thumbnail image
-  pictureImg.src = photoObject.url
-  pictureComments.innerText = photoObject.comments.length
-  pictureLikes.innerText = photoObject.likes
-
-  // finding element after which to add our thumbnail to the DOM
-  const imgUpload = document.querySelector(".img-upload")
-  // constructing fragment element to enhance our performance
-  let fragment = document.createDocumentFragment()
-  fragment.appendChild(pictureElement)
-  imgUpload.after(fragment)
-}
-
 export const displayAllThumbnails = function (photoObjects) {
-  photoObjects.map((item) => (displayThumbnail(item)))
+  const thumbnailTemplate = document.querySelector("#picture")
+
+  const fragment = document.createDocumentFragment()
+
+  photoObjects.forEach((item) => {
+    const pictureElement = thumbnailTemplate.cloneNode(true).content
+    const pictureImg = pictureElement.querySelector(".picture__img")
+    const pictureComments = pictureElement.querySelector(".picture__comments")
+    const pictureLikes = pictureElement.querySelector(".picture__likes")
+
+    pictureImg.src = item.url
+    pictureComments.innerText = item.comments.length
+    pictureLikes.innerText = item.likes
+
+    fragment.appendChild(pictureElement)
+  })
+  const imgUpload = document.querySelector(".img-upload")
+  imgUpload.after(fragment)
 }
