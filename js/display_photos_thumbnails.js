4f510bb0b7605cfd8d662eea44979ec122b0ca97
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2023-12-31 01:59:14 -0800

    add Photo full size view and comments

--- js/display_photos_thumbnails.js
+++ js/display_photos_thumbnails.js
@@ -5,10 +5,12 @@ export const displayAllThumbnails = function (photoObjects) {
 
   photoObjects.forEach((item) => {
     const pictureElement = thumbnailTemplate.cloneNode(true).content
+
     const pictureImg = pictureElement.querySelector(".picture__img")
     const pictureComments = pictureElement.querySelector(".picture__comments")
     const pictureLikes = pictureElement.querySelector(".picture__likes")
 
+    pictureImg.id = item.id
     pictureImg.src = item.url
     pictureComments.innerText = item.comments.length
     pictureLikes.innerText = item.likes
